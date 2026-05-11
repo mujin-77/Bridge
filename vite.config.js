8c769c2d98ed6461268e81fd36def1fc9ddbbe4e
欣 <1810062694@qq.com>
2026-05-11 08:30:05 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // 只保留 Vite 支持的配置
   plugins: [
     vue()
   ],
 
-  // 配置 @ 指向 src
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
 
-  // Less 全局变量自动引入
   css: {
     preprocessorOptions: {
       less: {
@@ -25,7 +21,6 @@ export default defineConfig({
     }
   },
 
-  // 本地开发跨域（部署后不影响）
   server: {
     proxy: {
       '/api': {
